0bc999b3439cd5b275cb7fed67f1ea524f11961f
yoan8306 <yoan8306@wanadoo.fr>
2021-11-12 08:18:30 +0100

    background mode ok

--- HorsZone/HorsZone/Controller/MapKitViewController.swift
+++ HorsZone/HorsZone/Controller/MapKitViewController.swift
@@ -21,7 +21,6 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     var counter = 0
     var startCheckPosition = false
     var polygonIdentity: [MKPolygon] = []
-    var background: UIBackgroundTaskIdentifier = .invalid
     
     // MARK: - IBoutlet
     @IBOutlet weak var mapView: MKMapView!
@@ -214,6 +213,7 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     }
     
     private func startMonitoringOff() {
+        locationManager.allowsBackgroundLocationUpdates = false
         startMonitoringButton.backgroundColor? = .init(red: 0, green: 0, blue: 0, alpha: 0)
         startCheckPosition = false
         timer?.invalidate()
@@ -221,6 +221,7 @@ class MapKitViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     
     /// initialize timer and launch checkPositionInPolygon()
     private func startMonitoringOn() {
+        locationManager.allowsBackgroundLocationUpdates = true
         startCheckPosition = true
         timer = Timer.scheduledTimer(timeInterval: 1, target: self,
                                      selector: #selector(checkPositionInPolygon), userInfo: nil, repeats: true)
